f97b98445c4c8d2f75d6ea2d92a7ccf60c98b921
Tomac Barin Jansson <tomacbarin@me.com>
2025-10-17 22:48:22 +0200

    Tried a few more JavaTricks and Clicks.

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+// First button
+// const buttonHtml = document.getElementById("veryButton");
+// buttonHtml.addEventListener("click", () => {
+//     alert("...and you just clicked it, amigo.");
+// })
+
 const buttonHtml = document.getElementById("veryButton");
-buttonHtml.addEventListener("click", () => {
-    alert("...and you just clicked it, amigo.");
+
+
+// buttonHtml.addEventListener("click", function (e) {
+//     console.log(e.target);
+// })
+
+buttonHtml.addEventListener("click", function (e) {
+    e.target.style.background = "black";
+    e.target.style.color = "#FF2B6F";
 })
